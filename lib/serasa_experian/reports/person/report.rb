be0706b412b73a27933e88a89b4ee9a4dc061b6e
CarHSMig <carlos.henrique@nullbug.dev>
2025-07-01 17:11:05 -0300

    correção para consulta de pf

--- lib/serasa_experian/reports/person/report.rb
+++ lib/serasa_experian/reports/person/report.rb
@@ -3,9 +3,9 @@
 require 'base64'
 module SerasaExperian
   module Reports
-    module Companies
+    module Person
       class Report < Reports::Base
-        REPORT_ENDPOINT = '/credit-services/business-information-report/v1/reports'
+        REPORT_ENDPOINT = '/credit-services/person-information-report/v1/creditreport'
       end
     end
   end
